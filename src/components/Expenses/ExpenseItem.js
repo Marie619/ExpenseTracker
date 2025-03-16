af2c67ef8094514b7f69ae41bc43b64c54238a9b
Marie619 <umairmajeed719@gmail.com>
2025-03-16 11:17:04 +0500

    incomplete delete feature

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,9 +1,23 @@
-import React from "react";
+import React,{useState} from "react";
 
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
+
+
+
 const ExpenseItem = (props) => {
+  const [deleteExpense,setDeleteExpense]= useState([props.expense]);
+
+  console.log(props.expense);
+
+  const deleteExpenseHandler = (event) =>{
+    const updatedExpense = deleteExpense.filter(item => item !==event.target.value);
+    setDeleteExpense(updatedExpense);
+    
+  }
+
+
   return (
     <li>
       <Card className="expense-item">
@@ -11,6 +25,7 @@ const ExpenseItem = (props) => {
         <div className="expense-item__description">
           <h2 className="expense-item__description h2">{props.title}</h2>
           <div className="expense-item__price">${props.amount}</div>
+          <div className="expense-item__delete" onClick={deleteExpenseHandler}>Delete</div>
         </div>
       </Card>
     </li>
